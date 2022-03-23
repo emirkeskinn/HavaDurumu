204b30cc676dd489c8951567bc1e5dbd9ed10c56
emirkeskinn <emkeskin41@gmail.com>
2022-03-23 22:44:34 +0300

    versiyon kontrolü test..

--- havaDurumu/DetailsViewController.swift
+++ havaDurumu/DetailsViewController.swift
@@ -8,22 +8,78 @@
 import UIKit
 
 class DetailsViewController: UIViewController {
-
+    @IBOutlet weak var imageViewTo: UIImageView!
+    
+    var city = String()
+    
+    @IBOutlet weak var cityName: UILabel!
+    @IBOutlet weak var weather: UILabel!
+    
+    @IBOutlet weak var sunrice: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        cityName.text = city
+        getTodayResult(cityName: city)
     }
+  
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.navigationBar.isHidden = false
     }
-    */
+  
+    func getTodayResult(cityName: String) {
+        
+        if let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=0f6112b1d663b03202ffabe9788c51ef") {
+            
+            let request = URLRequest(url: url)
+            
+            let task = URLSession.shared.dataTask(with: request) {(data,response,error) in
+                
+                if error == nil {
+                    if let incomingData = data {
+                        
+                        do {
+                            let json = try JSONSerialization.jsonObject(with: incomingData, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                            //print(json)
+                            
+                            if let main = json["main"] as? NSDictionary {
+                                if let temp = main["temp"] as? Double {
+                                    let nihaiSicaklik = Int(temp - 273.15)
+                                    
+                                    DispatchQueue.main.sync {
+                                        self.weather.text = String(nihaiSicaklik)
+                                        
+                                        if let sys = json["sys"] as? NSDictionary {
+                                            if let sunrise = sys["sunrise"] as? Double {
+                                                let gunDogumu = Int(sunrise)
+                                                DispatchQueue.main.sync {
+                                                    self.sunrice.text = String(gunDogumu)
+                                                }
+                                                    
+                                                
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            
+                        } catch {
+                            print("hata oluştu")
+                        }
+                        
+                    }
+                }
+                
+        }
+        
+            task.resume()
+    }
 
+        
+    
+    
+    
+}
 }
--- havaDurumu/ViewController.swift
+++ havaDurumu/ViewController.swift
@@ -8,12 +8,55 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var gosterButton: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        gosterButton.layer.cornerRadius = 16
+        textField.layer.cornerRadius = 16
     }
 
-
+  
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.navigationBar.isHidden = true
+        textField.text = ""
+    
+    }
+  
+     
+    @IBAction func gosterButton(_ sender: Any) {
+        
+        let city = textField.text ?? ""
+        
+        if city.isEmpty {
+            uyariMesaji(title: "UYARI", message: "Şehir ismi giriniz")
+            
+        } else {
+            
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: "DetailsViewController") as! DetailsViewController
+            
+            vc.city = city
+            
+            self.show(vc, sender: nil)
+            
+        }
+        
+        
+      //  performSegue(withIdentifier: "toDetailsVC", sender: nil)
+        
+    }
+    
+    func uyariMesaji(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+    }
+   
+    
 }
 
